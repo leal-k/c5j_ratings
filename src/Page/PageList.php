10f80fc53d5a6f18ae01924450026f5d5b0a3b49
aye <aye@concrete5.co.jp>
2021-11-24 11:30:17 +0900

    add additional filters & orders

--- src/Page/PageList.php
+++ src/Page/PageList.php
@@ -5,21 +5,38 @@
  */
 
 namespace C5jRatings\Page;
+use Doctrine\DBAL\Types\Type;
 
 class PageList extends \Concrete\Core\Page\PageList
 {
-    public function filterByUserRated($uID): void
+    public function __construct()
     {
-        // TODO::implementation
+        parent::__construct();
+        $this->query->leftJoin('p', 'C5jRatings', 'r', 'p.cID = r.cID and r.ratedValue = 1');
+        $this->query->addSelect('SUM(r.ratedValue) AS ratings');
+        $this->query->addSelect('r.bID');
+        $this->query->addGroupBy('r.cID');
+
+    }
+
+    public function filterByUserRated(int $uID): void
+    {
+        $this->query->andWhere('r.uID = :uID');
+        $this->query->setParameter('uID', $uID, Type::INTEGER);
     }
 
     public function filterByMinimumNumberOfRatings(int $num = 0): void
     {
-        // TODO::implementation
+        $this->query->andHaving($this->query->expr()->gte('SUM(r.ratedValue)', $num));
     }
 
     public function sortByMostRated(): void
     {
-        // TODO::implementation
+        $this->query->orderBy('ratings', 'DESC');
+    }
+
+    public function getResult($queryRow)
+    {
+        return $queryRow;
     }
 }
