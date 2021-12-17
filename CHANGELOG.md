319c51712262008beeb8714e7c8861e353cbc0e0
Md Biplob Hossain <biplob.ice@gmail.com>
2021-12-17 14:14:38 +0900

    Update version & CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## [0.1.0] - 2021-12-17
+- First release
--- controller.php
+++ controller.php
@@ -29,7 +29,7 @@ class Controller extends Package
     /**
      * @var string package version
      */
-    protected $pkgVersion = '0.1.0-a';
+    protected $pkgVersion = '0.1.0';
 
     protected $pkgAutoloaderRegistries = [
         'src' => '\C5jRatings',
