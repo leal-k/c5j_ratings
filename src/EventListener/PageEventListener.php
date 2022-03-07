3c79ee8bf044df819edb7adc7ef24bba83a7162a
Md Biplob Hossain <biplob.ice@gmail.com>
2022-03-07 19:28:44 +0900

    php-cs-fixer

--- src/EventListener/PageEventListener.php
+++ src/EventListener/PageEventListener.php
@@ -3,24 +3,25 @@
 namespace C5jRatings\EventListener;
 
 use C5jRatings\Entity\C5jRating;
+use Concrete\Core\Page\DeletePageEvent;
 use Concrete\Core\Support\Facade\Application;
 use Doctrine\ORM\EntityManagerInterface;
 
 class PageEventListener
 {
-    public function onPageDelete(\Concrete\Core\Page\DeletePageEvent $event): void
+    public function onPageDelete(DeletePageEvent $event): void
     {
-		$page = $event->getPageObject();
-		if($page){
-			$cID = $page->getCollectionID();
-			$app = Application::getFacadeApplication();
-			/** @var EntityManagerInterface $em */
-			$em = $app->make(EntityManagerInterface::class);
-			/** @var C5jRating $rating */
-			$ratings = $em->getRepository(C5jRating::class)->findBy(['cID' => $cID]);
-			foreach ($ratings as $rating) {
-				$rating->delete();
-			}
-		}
+        $page = $event->getPageObject();
+        if ($page) {
+            $cID = $page->getCollectionID();
+            $app = Application::getFacadeApplication();
+            /** @var EntityManagerInterface $em */
+            $em = $app->make(EntityManagerInterface::class);
+            /** @var C5jRating $rating */
+            $ratings = $em->getRepository(C5jRating::class)->findBy(['cID' => $cID]);
+            foreach ($ratings as $rating) {
+                $rating->delete();
+            }
+        }
     }
 }
--- src/EventListener/UserEventListener.php
+++ src/EventListener/UserEventListener.php
@@ -4,23 +4,24 @@ namespace C5jRatings\EventListener;
 
 use C5jRatings\Entity\C5jRating;
 use Concrete\Core\Support\Facade\Application;
+use Concrete\Core\User\Event\DeleteUser;
 use Doctrine\ORM\EntityManagerInterface;
 
 class UserEventListener
 {
-	public function onUserDelete(\Concrete\Core\User\Event\DeleteUser $event): void
-	{
-		$ui = $event->getUserInfoObject();
-		if($ui){
-			$uID = $ui->getUserID();
-			$app = Application::getFacadeApplication();
-			/** @var EntityManagerInterface $em */
-			$em = $app->make(EntityManagerInterface::class);
-			/** @var C5jRating $rating */
-			$ratings = $em->getRepository(C5jRating::class)->findBy(['uID' => $uID]);
-			foreach ($ratings as $rating) {
-				$rating->delete();
-			}
-		}
-	}
+    public function onUserDelete(DeleteUser $event): void
+    {
+        $ui = $event->getUserInfoObject();
+        if ($ui) {
+            $uID = $ui->getUserID();
+            $app = Application::getFacadeApplication();
+            /** @var EntityManagerInterface $em */
+            $em = $app->make(EntityManagerInterface::class);
+            /** @var C5jRating $rating */
+            $ratings = $em->getRepository(C5jRating::class)->findBy(['uID' => $uID]);
+            foreach ($ratings as $rating) {
+                $rating->delete();
+            }
+        }
+    }
 }
