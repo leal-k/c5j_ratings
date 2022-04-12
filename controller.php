a3ead28e8f52a219650fa6e72877d2339ddad976
aye <aye@macareux.co.jp>
2022-04-12 16:02:16 +0900

    fixed "Failed to create closure from callable: non-static method C5jRatings\EventListener\PageEventListener::onPageDelete() should not be called statically"

--- controller.php
+++ controller.php
@@ -76,8 +76,14 @@ class Controller extends Package
         $this->registerAssets();
 
 		$director = $this->app->make('director');
-		$director->addListener('on_page_delete', [PageEventListener::class, 'onPageDelete']);
-		$director->addListener('on_user_delete', [UserEventListener::class, 'onUserDelete']);
+		$director->addListener('on_page_delete', function ($e) {
+			$ratingPageEvent = $this->app->make(PageEventListener::class);
+			$ratingPageEvent->onPageDelete($e);
+		});
+		$director->addListener('on_user_delete', function ($e) {
+			$ratingUserEvent = $this->app->make(UserEventListener::class);
+			$ratingUserEvent->onUserDelete($e);
+		});
     }
 
     private function installXml(): void
